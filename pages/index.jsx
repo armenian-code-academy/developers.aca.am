26901fe859bcc3b1d14ce2b7c17838e44eef3d15
Albert Barsegyan <albertbarsegyan6@gmail.com>
2021-09-28 20:24:23 +0400

    feat(*): save current position

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <div>
-        <span>developers</span>
+        <span className="text-xl text-green-500">developers</span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   purge: [],
 
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
