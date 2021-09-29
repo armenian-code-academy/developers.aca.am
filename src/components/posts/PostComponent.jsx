e400e85dc4378dddb22794bb214c37353790ed75
Albert <Albert@Macs-MacBook-Pro.local>
2021-09-29 21:12:22 +0400

    feat(PostList.jsx): add grid list

--- src/components/posts/PostComponent.jsx
+++ src/components/posts/PostComponent.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Image from 'next/image';
 export default function PostComponent() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="relative w-full h-48">
+        <Image
+          alt="content"
+          src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
+          layout="fill"
+        />
+      </div>
+      <div className="my-2 text-textLight">
+        <p>Lorem ipsum dolor sit.</p>
+      </div>
+    </div>
+  );
 }
