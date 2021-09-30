6c56d7ee83567d0e3360a9f9dc9e39ea92d79f2c
Albert Barsegyan <albertbarsegyan6@gmail.com>
2021-09-30 21:58:31 +0400

    feat(content): add content folder for markdown data

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,3 +22,10 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
