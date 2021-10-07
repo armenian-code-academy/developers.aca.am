8b275f7fc7c42f2c00159189f7d98a6ca4525b73
albertBarsegyan <albertbarsegyan6@gmail.com>
2021-10-07 22:12:08 +0400

    fix(blog): blog armenian mode doesnt work well

--- pages/careers.jsx
+++ pages/careers.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Head from 'next/head';
+import PostHeader from '../src/components/posts/PostHeader';
+import PostList from '../src/components/posts/PostList';
+import Footer from '../src/components/sections/Footer';
+import Header from '../src/components/sections/Header';
+import Wrapper from '../src/components/wrappers/Wrapper';
+import Locales from '../src/components/sections/Locales';
+import PrimaryDescription from '../src/components/sections/PrimaryDescription';
 
 export default function Careers() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>ACA Careers</title>
+      </Head>
+      <Locales />
+      <Header />
+      <Wrapper>
+        <PrimaryDescription
+          header="Lorem ipsum"
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius,
+                  aliquid."
+        />
+        <PostHeader />
+        <PostList />
+        <Footer />
+      </Wrapper>
+    </div>
+  );
 }
